e4c52296081f6ddf9edcf078e458ce888c550fd8
Nahuel <nahuelparedes0@gmail.com>
2023-10-22 22:25:48 -0300

    javascript

--- scripts/main.js
+++ scripts/main.js
@@ -39,37 +39,37 @@ while ( stock.trim().toUpperCase() !=="FIN" && stock.trim() !== "" ){
 /* segunda pre entrega */
 
 /* objeto */
-let stock = {
+/* let stock = {
   prenda: 0,
   talle: 0,
   color: 0,
-  calidad: 0,
+  calidad: 0, */
 
-};
-stock.prenda = prompt("ingrese la prenda a reponer");
+/* } */;
+/* stock.prenda = prompt("ingrese la prenda a reponer");
 stock.talle = prompt("ingrese los talles a reponer");
 stock.color = prompt("ingrese el color de la prenda");
 stock.calidad = prompt("ingrese la calidad de la prenda");
-console.log(" los  datos son ", stock)
+console.log(" los  datos son ", stock) */
 /*  */
 /* array */
-let iventario = [];
+/* let iventario = [];
 let repuesta = prompt("¿Quieres agregar alguna prenda al stock?, si no lo desea comentar un NO")
 while (repuesta.trim().toUpperCase() === "SI") {
   let unInventario = prompt("¿Que prenda desea agregar al stock?")
   iventario.push(unInventario)
   repuesta = prompt("¿Quieres seguir?")
 
-}
-console.log("las prendas a pedir son")
+} */
+/* console.log("las prendas a pedir son")
 for (let index = 0; index < iventario.length; index++) {
   console.log(index + " " + iventario[index] + " a pedir"
-  )
+  ) */
 
-}
+/* } */
 /*  */
 /* metodo de busqueda dentro de un array */
-const marcaSolicitada = prompt("ingrese la marca del producto")
+/* const marcaSolicitada = prompt("ingrese la marca del producto")
 
 const prendaFiltrada = prendasMock.filter((unaMarca) =>
   unaMarca.marca
@@ -78,5 +78,15 @@ const prendaFiltrada = prendasMock.filter((unaMarca) =>
     .includes(marcaSolicitada.trim().toLowerCase())
 
 )
-console.table(prendaFiltrada)
+console.table(prendaFiltrada) */
 /*  */
+
+
+const FORMULARIO = document.getElementById("prenda")
+FORMULARIO.addEventListener("submit", (event) => {
+  event.preventdefault();
+  let mensaje= FORMULARIO.children[1].value
+  console.log (mensaje)
+ 
+})
+
